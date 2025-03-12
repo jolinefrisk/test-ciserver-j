062952f03b8c45079975f6dfc79fc6b91538d397
jolinefrisk <112376484+jolinefrisk@users.noreply.github.com>
2025-03-12 13:47:32 +0100

    Update App.java

--- src/main/java/com/exempel/App.java
+++ src/main/java/com/exempel/App.java
@@ -6,6 +6,6 @@ package com.exempel;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!2");
+        System.out.println("Hello World!");
     }
 }
